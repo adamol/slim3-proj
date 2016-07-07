8d2abb10588d47809b5ee2e09b162658fd16edc1
adamol <adamol1992@gmail.com>
2016-07-07 21:22:45 +0200

    Use the posted form data to create users in database

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\Auth;
 
 use App\Controllers\Controller;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -11,8 +12,14 @@ class AuthController extends Controller
 		return $this->view->render($response, 'auth/signup.twig');
 	}
 
-	public function postSignUp()
+	public function postSignUp($request, $response)
 	{
-		
+		$user = User::create([
+			'email' => $request->getParam('email'),
+			'name' => $request->getParam('name'),
+			'password' => password_hash($request->getParam('password'), PASSWORD_DEFAULT),
+		]);
+
+		return $response->withRedirect($this->router->pathFor('home'));
 	}
 }
