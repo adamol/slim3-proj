7ede3c4f70d450ce98031edba4309909984a1999
adamol <adamol1992@gmail.com>
2016-07-07 21:12:08 +0200

    Add AuthController and create signup form

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,18 +2,10 @@
 
 namespace App\Controllers;
 
-use Slim\Views\Twig as View;
-use App\Models\User;
-
 class HomeController extends Controller
 {
 	public function index($request, $response)
 	{
-		User::create([
-			'name' => 'Billy bob',
-			'email' => 'billy@bob.com',
-			'password' => 'password'
-		]);
 		return $this->view->render($response, 'home.twig');
 	}
 }
